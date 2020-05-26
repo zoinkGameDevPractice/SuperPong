92c68eeb72eacd07f281922192e2493829b866fc
zoinkGameDevPractice <zoinkGameDevPractice@users.noreply.github.com>
2020-05-25 21:53:42 -0400

    New System
    Added a new system that applies a velocity boost to the ball if its velocity falls under a certain threshold.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,6 +6,9 @@ public class Ball : MonoBehaviour
 {
     public float speed = 5f;
     Rigidbody2D rb;
+    [HideInInspector]
+    public bool needsBoost = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,18 @@ public class Ball : MonoBehaviour
         float y = Random.Range(1f, 2f);
 
         rb.velocity = new Vector2(x, y) * speed;
+    }
+
+    private void Update()
+    {
+        if (rb.velocity.x < 2f)
+            needsBoost = true;
+        else
+            needsBoost = false;
+    }
+
+    public void ApplyBoost(Vector2 boost)
+    {
+        rb.velocity += boost;
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,13 +7,17 @@ public class Movement : MonoBehaviour
     public float speed = 5f;
 
     Rigidbody2D rb;
+    Ball ball;
 
     bool isPlayerOne;
     Vector2 velocity;
 
+    public float boost = 2.5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        ball = GameObject.FindWithTag("Ball").GetComponent<Ball>();
         rb = GetComponent<Rigidbody2D>();
         if (tag == "Player1")
             isPlayerOne = true;
@@ -34,5 +38,12 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D collider = collision.collider;
+        if (collider.CompareTag("Ball"))
+            ball.ApplyBoost(new Vector2(boost, 0));
     }
 }
