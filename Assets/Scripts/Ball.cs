ee78a782b5a54dd70c3b12fb81d531cbeeecdb60
zoinkGameDevPractice <zoinkGameDevPractice@users.noreply.github.com>
2020-05-25 21:38:42 -0400

    Added physics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Launch();
+    }
+
+    void Launch()
+    {
+        float x = Random.Range(1f, 2f);
+        float y = Random.Range(1f, 2f);
+
+        rb.velocity = new Vector2(x, y) * speed;
+    }
+}
