12a5ed5ae2c2c640bd9247734edb2f98e944ecb6
zoinkGameDevPractice <zoinkGameDevPractice@users.noreply.github.com>
2020-05-27 21:02:09 -0400

    Main menu script and Data object

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public Data instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public int pointsToWin;
+    public int playerSpeed;
+    public int ballSpeed;
+
+    public float musicVolume;
+    public float soundVolume;
+}
